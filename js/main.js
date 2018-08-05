7adcf34d73a85e48339a1d4f87797d02971f109a
Brittany Bellanca <brittanybellanca@gmail.com>
2018-08-05 18:26:15 -0400

    added modal

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.drawer').drawer();
 });
 
-
+// Removed Disabled Attribute & Add button--primary when form is completed
 $(function() {
   $('.input').keyup(function() {
 
@@ -21,3 +21,30 @@ $(function() {
     }
   });
 }());
+
+// Modal JS
+var modal = document.getElementById('modal');
+    button = document.getElementById("modal__button--mobile");
+    buttonDesktop = document.getElementById("modal__button--desktop");
+    icon = document.getElementsByClassName("modal__close")[0];
+
+// Listen for a click on either the button or link to open the modal
+button.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+
+buttonDesktop.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+
+// Listen for a click on icon to close the modal
+icon.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+// Click outside of modal = modal will close
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
