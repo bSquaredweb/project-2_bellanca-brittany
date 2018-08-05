f88aa2d4b8d66f82a05b5bf2cbbf4bad5936582d
Brittany Bellanca <brittanybellanca@gmail.com>
2018-08-05 16:29:00 -0400

    added jQuery for disabled submit button on form

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
 $(document).ready(function() {
   $('.drawer').drawer();
 });
+
+
+$(function() {
+  $('.input').keyup(function() {
+
+    var empty = false;
+    $('.input').each(function() {
+      if ($(this).val() == '') {
+        empty = true;
+      }
+    });
+
+    if (empty) {
+      $('#submit').hasClass('.button--disabled');
+    } else {
+      $('#submit').removeAttr('disabled').addClass('button--primary').removeClass('button--disabled');
+    }
+  });
+}());
