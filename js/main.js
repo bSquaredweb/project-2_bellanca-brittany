e0d545952a2a5ea11ee186079a4eab332772e1b6
Brittany Bellanca <brittanybellanca@gmail.com>
2018-08-08 20:33:42 -0400

    added mixins and updated code per deductions page

--- js/main.js
+++ js/main.js
@@ -5,45 +5,45 @@ $(document).ready(function() {
 
 // Custom jQuery - Click on Circle/Icon to reveal Text below through CSS changes
 $(document).ready(function() {
-    $('.interactive-element__circle--1').click(function() {
-      if ( $('.interactive-element__text--1').hasClass('hidden-text') ) {
-        $('.interactive-element__text--1').removeClass('hidden-text').addClass('visible-text');
-      } else {
-        $('.interactive-element__text--1').addClass('hidden-text').removeClass('visible-text');
-      }
-    });
+  $('.interactive-element__circle--1').click(function() {
+    if ( $('.interactive-element__text--1').hasClass('hidden-text') ) {
+      $('.interactive-element__text--1').removeClass('hidden-text').addClass('visible-text');
+    } else {
+      $('.interactive-element__text--1').addClass('hidden-text').removeClass('visible-text');
+    }
+  });
 
-    $('.interactive-element__circle--2').click(function() {
-      if ( $('.interactive-element__text--2').hasClass('hidden-text') ) {
-        $('.interactive-element__text--2').removeClass('hidden-text').addClass('visible-text');
-      } else {
-        $('.interactive-element__text--2').addClass('hidden-text').removeClass('visible-text');
-      }
-    });
+  $('.interactive-element__circle--2').click(function() {
+    if ( $('.interactive-element__text--2').hasClass('hidden-text') ) {
+      $('.interactive-element__text--2').removeClass('hidden-text').addClass('visible-text');
+    } else {
+      $('.interactive-element__text--2').addClass('hidden-text').removeClass('visible-text');
+    }
+  });
 
-    $('.interactive-element__circle--3').click(function() {
-      if ( $('.interactive-element__text--3').hasClass('hidden-text') ) {
-        $('.interactive-element__text--3').removeClass('hidden-text').addClass('visible-text');
-      } else {
-        $('.interactive-element__text--3').addClass('hidden-text').removeClass('visible-text');
-      }
-    });
+  $('.interactive-element__circle--3').click(function() {
+    if ( $('.interactive-element__text--3').hasClass('hidden-text') ) {
+      $('.interactive-element__text--3').removeClass('hidden-text').addClass('visible-text');
+    } else {
+      $('.interactive-element__text--3').addClass('hidden-text').removeClass('visible-text');
+    }
+  });
 
-    $('.interactive-element__circle--4').click(function() {
-      if ( $('.interactive-element__text--4').hasClass('hidden-text') ) {
-        $('.interactive-element__text--4').removeClass('hidden-text').addClass('visible-text');
-      } else {
-        $('.interactive-element__text--4').addClass('hidden-text').removeClass('visible-text');
-      }
-    });
+  $('.interactive-element__circle--4').click(function() {
+    if ( $('.interactive-element__text--4').hasClass('hidden-text') ) {
+      $('.interactive-element__text--4').removeClass('hidden-text').addClass('visible-text');
+    } else {
+      $('.interactive-element__text--4').addClass('hidden-text').removeClass('visible-text');
+    }
+  });
 
-    $('.interactive-element__circle--5').click(function() {
-      if ( $('.interactive-element__text--5').hasClass('hidden-text') ) {
-        $('.interactive-element__text--5').removeClass('hidden-text').addClass('visible-text');
-      } else {
-        $('.interactive-element__text--5').addClass('hidden-text').removeClass('visible-text');
-      }
-    });
+  $('.interactive-element__circle--5').click(function() {
+    if ( $('.interactive-element__text--5').hasClass('hidden-text') ) {
+      $('.interactive-element__text--5').removeClass('hidden-text').addClass('visible-text');
+    } else {
+      $('.interactive-element__text--5').addClass('hidden-text').removeClass('visible-text');
+    }
+  });
 });
 
 // Custom jQuery - Removes Disabled Attribute & Add button--primary when form is completed
@@ -99,7 +99,7 @@ icon.addEventListener('click', function() {
 
 // Click outside of modal = modal will close
 window.addEventListener('click', function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+      modal.style.display = "none";
+  }
 });
